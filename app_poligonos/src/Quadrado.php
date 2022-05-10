9a19dfb8e308aadea031579fec42073302bcbfc8
Elton Andrade <eltonsa75@hotmail.com>
2022-05-10 11:29:40 -0300

    Projeto Polígonos - Retângulo e Quadrado

---
+++ app_poligonos/src/Quadrado.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace src;
+
+
+class Quadrado extends Retangulo {
+
+    public function setLargura(float $largura): void {
+        $this->largura = $largura;
+        $this->altura = $largura;
+    }
+    public function setAltura(float $altura): void {
+        $this->largura = $altura;
+        $this->altura = $altura;
+    }
+}
