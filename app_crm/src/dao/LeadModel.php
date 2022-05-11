a4d732ec6b9357796622eb8fbdc3ff5dfa5e5b5c
Elton Andrade <eltonsa75@hotmail.com>
2022-05-11 09:12:56 -0300

    Projeto CRM - Implementando a interface

--- app_crm/src/dao/LeadModel.php
+++ app_crm/src/dao/LeadModel.php
@@ -4,7 +4,21 @@
 namespace src\dao;
 
 use src\BD;
+use src\interfaces\ICadastro;
 
-class LeadModel extends BD {
+use src\componentes\Log;
+use src\componentes\Notificacao;
+
+class LeadModel extends BD implements ICadastro {
+    public function salvar() {
+        //lógica
+    }
     
+    public function registrarLog(Log $log) {
+        //lógica
+    }
+
+    public function enviarNotificacao(Notificacao $notificacao) {
+        //lógica
+    }
 }
--- app_crm/src/dao/UsuarioModel.php
+++ app_crm/src/dao/UsuarioModel.php
@@ -5,6 +5,21 @@ namespace src\dao;
 
 use src\BD;
 
-class UsuarioModel extends BD {
-    
+use src\interfaces\ICadastro;
+
+use src\componentes\Log;
+use src\componentes\Notificacao;
+
+class UsuarioModel extends BD implements ICadastro {
+    public function salvar() {
+        //lógica
+    }
+
+    public  function registrarLog(Log $log) {
+        //lógica
+    }
+
+    public function enviarNotificacao(Notificacao $notificacao) {
+        //lógica
+    }
 }
