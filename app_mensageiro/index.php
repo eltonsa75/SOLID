ce441ad694fe85780e0cb4e63f463ee84209058c
Elton Andrade <eltonsa75@hotmail.com>
2022-05-11 11:00:04 -0300

    Projeto Mensageiro parte 1 - Implementando os componentes da aplicação

---
+++ app_mensageiro/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+require __DIR__.'/vendor/autoload.php';
+
+use src\Mensageiro;
+
+$msg = new Mensageiro();
+$msg->enviarToken();
---
+++ app_mensageiro/src/Email.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace src;
+
+
+class Email {
+    public function enviar(): void {
+        echo 'E-mail: Seu token é 555-333';
+    }
+}
