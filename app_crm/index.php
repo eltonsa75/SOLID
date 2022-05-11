fdf09394bdf07dc223a00d3eb949a96250396af4
Elton Andrade <eltonsa75@hotmail.com>
2022-05-11 09:09:09 -0300

    Projeto CRM - Implementando a interface

--- app_crm/index.php
+++ app_crm/index.php
@@ -3,4 +3,19 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-echo 'teste ISP';
+use src\dao\UsuarioModel;
+use src\dao\ContatoModel;
+use src\dao\LeadModel;
+
+$contratoModel = new ContatoModel();
+print_r($contratoModel);
+echo '<br>';
+
+
+$lealdModel = new LeadModel();
+print_r($lealdModel);
+echo '<br>';
+
+$usuarioModel = new UsuarioModel();
+print_r($usuarioModel);
+echo '<br>';
--- app_crm/src/dao/ContatoModel.php
+++ app_crm/src/dao/ContatoModel.php
@@ -4,7 +4,21 @@
 namespace src\dao;
 
 use src\BD;
+use src\interfaces\ICadastro;
 
-class ContatoModel extends BD {
-    
+use src\componentes\Log;
+use src\componentes\Notificacao;
+
+class ContatoModel extends BD implements ICadastro {
+    public function salvar() {
+        //lógica
+    }
+
+    public function registrarLog(Log $log) {
+        //lógica
+    }
+
+    public function enviarNotificacao(Notificacao $notificacao) {
+        //lógica
+    }
 }
