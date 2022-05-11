4be73f2c64efcb8d9f6e99254a53326098cc94aa
Elton Andrade <eltonsa75@hotmail.com>
2022-05-11 08:40:11 -0300

    Interface Segregation Principle (Pricnipio da Segregação de Interface)

--- app_poligonos/index.php
+++ app_poligonos/index.php
@@ -24,18 +24,3 @@ echo '<pre>';
 print_r($poligono);
 echo '</pre>';
 echo '<h3>LSP - Área do Quadrado: '.$poligono->getArea().'</h3>';
-
-// $retangulo = new Retangulo();
-// $retangulo->setAltura(5);
-// $retangulo->setLargura(10);
-// echo '<h3>Área do retângulo: '.$retangulo->getArea().'</h3>';
-
-
-// $quadrado = new Quadrado();
-// $quadrado->setAltura(5);
-// echo '<h3>Área do quadrado: '.$quadrado->getArea().'</h3>';
-
-// $retangulo = new Quadrado();
-// $retangulo->setAltura(5);
-// $retangulo->setLargura(10);
-
